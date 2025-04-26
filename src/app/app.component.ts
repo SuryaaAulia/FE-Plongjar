243cdc4c19102952c1f8802cbe61ee0a5ccf3839
SuryaaAulia <suryaaulia489@gmail.com>
2025-04-26 16:58:11 +0700

    Update - Essential Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
-import { BodyLayoutComponent } from './layouts/body-layout/body-layout.component';
-import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
-import { NavItem, SideNavToggle } from './core/models/nav-item.model';
-import { NavService } from './core/services/nav.service';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidenavComponent, MainLayoutComponent],
+  imports: [RouterOutlet, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'FE-Plongjar';
-
-  screenWidth = 0;
-  collapsed = false;
-  menuItems: NavItem[] = [];
-
-  constructor(private navService: NavService) {}
-
-  ngOnInit() {
-    this.navService.getMenu('admin').subscribe(items => {
-      this.menuItems = items;
-    });
-  }
-
-  onToggleSideNav(data: SideNavToggle) {
-    this.screenWidth = data.screenWidth;
-    this.collapsed = data.collapsed;
-  }
 }
