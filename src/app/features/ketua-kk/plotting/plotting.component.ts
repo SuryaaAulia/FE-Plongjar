d752acb9d451ba010c0c173b97938e988c1baffb
SuryaaAulia <suryaaulia489@gmail.com>
2025-05-03 23:34:36 +0700

    Update - Ketua KK pages

---
+++ src/app/features/ketua-kk/plotting/plotting.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plotting',
+  standalone: true,
+  imports: [],
+  templateUrl: './plotting.component.html',
+  styleUrl: './plotting.component.scss'
+})
+export class PlottingComponent {
+
+}
