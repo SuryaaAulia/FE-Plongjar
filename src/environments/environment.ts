2ba42a471ff22ca2cb0b2505828fe327bbe53f8e
SuryaaAulia <suryaaulia489@gmail.com>
2025-07-01 04:17:04 +0700

    Update - environment
    update apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://api.siplongjar.com',
+    apiUrl: 'http://api.siplongjar.com/api/v1',
     appName: 'Apps-SoC',
     tokenKey: 'auth_token',
     userKey: 'auth_user',
