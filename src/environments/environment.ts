834c2afb6e4b87fa704b3a27373c7c4dc08d77a8
SuryaaAulia <suryaaulia489@gmail.com>
2025-07-30 00:10:25 +0700

    Update - Environment
    change the apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://siplongjar.com/api/v1',
+    apiUrl: 'https://siplongjar.com/api/v1',
     appName: 'Apps-SoC',
     tokenKey: 'auth_token',
     userKey: 'auth_user',
