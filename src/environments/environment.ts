9ff7f57f4a9b62212df4637a0bbd96c0dba30a4e
SuryaaAulia <suryaaulia489@gmail.com>
2025-09-04 16:35:30 +0700

    Teehe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://siplongjar.com/api/v1',
+    // apiUrl: 'https://siplongjar.com/api/v1',
+    apiUrl: 'http://127.0.0.1:8000/api/v1',
     appName: 'Apps-SoC',
     tokenKey: 'auth_token',
     userKey: 'auth_user',
