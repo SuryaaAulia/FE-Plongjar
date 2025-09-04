83d216589ccb3f633a40b34f81f2e5eb9be9fbae
SuryaaAulia <suryaaulia489@gmail.com>
2025-09-04 16:34:22 +0700

    Update - Component Dashboard
    adjust the styling to fit Angular and add new button for user manual

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ActionButtonComponent } from '../../shared/components';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ActionButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  openUserManual() {
+    const pdfUrl = 'assets/user_manual/User Manual Siplongjar V1.pdf';
+    window.open(pdfUrl, '_blank');
+  }
 }
